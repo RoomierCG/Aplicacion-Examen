348c6f3c77074e384d3896401a14cac7603b2b77
Edgar Alexander Calle Granda <alex.comcg@gmail.com>
2020-02-20 17:23:30 +0100

    Adapter creado, ahora ya se pueden ver a los usuario creados

---
+++ MadLab/app/src/main/java/com/roomiercg/madlab/Adapter/UserAdapter.java
@@ -0,0 +1,70 @@
+package com.roomiercg.madlab.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.roomiercg.madlab.Model.User;
+import com.roomiercg.madlab.R;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<User> mUser;
+
+    public UserAdapter(Context mContext, List<User> mUser){
+        this.mContext = mContext;
+        this.mUser = mUser;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item,parent,false);
+        return new UserAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        User user = mUser.get(position);
+        holder.username.setText(user.getUsername());
+        if(user.getImageURL().equals("default")){
+            holder.profile_image.setImageResource(R.mipmap.ic_launcher);
+        }else{
+            Glide.with(mContext).load(user.getImageURL()).into(holder.profile_image);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUser.size();
+    }
+
+    /*
+        Almacenamos en esta clase las foto de los usuarios que esten conectados
+     */
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView username;
+        public ImageView profile_image;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            username = itemView.findViewById(R.id.username);
+            profile_image = itemView.findViewById(R.id.profile_image);
+
+
+        }
+    }
+}
